eaf6e9bdcad4b133e7060feef35c8e8ca08e0770
wayne71 <wayne.hatter1971@gmail.com>
2024-04-04 03:36:43 -0500

    Insert a single record

--- PyMongo/new_account.py
+++ PyMongo/new_account.py
@@ -1,44 +1,3 @@
-# Using Python with MongoDB
-## MongoDB connection
-```python
-from  dotenv import load_dotenv
-from pymongo import MongoClient
-
-# Load config from .env file
-load_dotenv()
-
-MONGODB_URI = os.environ["MONGODB_URI"]
-
-client = MongoClient(MONGODB_URI)
-
-for db_name in client.list_database_names():
-    print(db_name)
-
-client.close()
-```
-# MongoDB CRUD Operations in Python
-## Working with MongoDB documents
-```python
-new_document = {
-    "account_holder": "Addison Shelton",
-    "account_id": "MDB955769550",
-    "account_type": "checking",
-    "years_active": 5,
-    "address": {
-        "city": "Ridgewook",
-        "zip": "11385",
-        "street": "Menahan St",
-        "number": "1712",
-    },
-    "transfer_complete": [
-        "TR28105502",
-        "TR197586149",
-        "TR586833243",
-    ]
-}
-```
-## Insert a single record
-```python
 import datetime
 import os
 
@@ -74,4 +33,3 @@ document_ids = result.inserted_id
 print(f"_id of inserted document: {document_ids}")
 
 client.close()
-```
